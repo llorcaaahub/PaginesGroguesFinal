6e99bdef6747b3f0e5d5af726f6ebad68b7d2b2f
BlaiMM <blai.menchenm@educem.net>
2022-11-05 12:04:03 +0100

    he avançat amb Bussines

---
+++ app/src/main/java/com/example/paginesgrogues/BussinesActivity.java
@@ -0,0 +1,34 @@
+package com.example.paginesgrogues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import android.widget.Toast;
+
+public class BussinesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bussines);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_tipus_bussines);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tipus_de_bussines, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
+    }
+}
+
