2f6b0e9bcd47549f9551584da500279750309a3b
BlaiMM <blai.menchenm@educem.net>
2022-11-05 12:03:38 +0100

    he avançat amb Bussines

--- app/src/main/java/com/example/paginesgrogues/RestaurantsActivity.java
+++ app/src/main/java/com/example/paginesgrogues/RestaurantsActivity.java
@@ -19,7 +19,7 @@ public class RestaurantsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
 
-        String[] restaurants = { "India", "USA", "China", "Japan", "Other"};
+
 
 
 
