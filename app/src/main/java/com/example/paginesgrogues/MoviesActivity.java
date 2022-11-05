c3df634308ee3a56add9498fd919262eda6f552d
BlaiMM <blai.menchenm@educem.net>
2022-11-05 20:39:33 +0100

    ultima actualitzacio del dissabte

--- app/src/main/java/com/example/paginesgrogues/MoviesActivity.java
+++ app/src/main/java/com/example/paginesgrogues/MoviesActivity.java
@@ -1,6 +1,13 @@
 package com.example.paginesgrogues;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MoviesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
 }
