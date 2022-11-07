08d7eec6c44d156837a49381aaebcad260fca56c
BlaiMM <blai.menchenm@educem.net>
2022-11-07 10:25:45 +0100

    tenim lectura de JSON tt

--- app/src/main/java/com/example/paginesgrogues/BussinesActivity.java
+++ app/src/main/java/com/example/paginesgrogues/BussinesActivity.java
@@ -3,19 +3,29 @@ package com.example.paginesgrogues;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BussinesActivity extends AppCompatActivity {
+
+    TextView t1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_bussines);
 
+        t1 = findViewById(R.id.t1);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner_tipus_bussines);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -32,20 +42,30 @@ public class BussinesActivity extends AppCompatActivity {
 
     public String loadJSONFromAsset() {
         String json = null;
+
+
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("bussines.json")));
             String paraula = "exemple";
 
             // do reading, usually loop until end of file reading
+            StringBuilder sb = new StringBuilder();
             String mLine;
-            while ((mLine = reader.readLine()) != null) {
+            while ((mLine= reader.readLine()) != null) {
                 //process line
-                mLine = mLine + ".";
-            }
+                sb.append(mLine);
 
-        } catch (IOException e) {
+
+            }
+            JSONArray exemple = new JSONArray(sb.toString());
+            String  numero = "ii";
+            t1.setText((CharSequence) exemple.getJSONObject(0).get("nomEmpresa"));
+
+        } catch (IOException | JSONException e) {
             //log the exception
         }
+
+
 
         String paraula = "Exemple";
         return paraula;
