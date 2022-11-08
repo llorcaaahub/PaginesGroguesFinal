57584c02fac60fbe16d4c53b512a6188afd2a4c6
llorca <marc.llorcal@educem.net>
2022-11-08 10:57:28 +0100

    Fer la major part de restaurants.json automatitzat

--- app/src/main/java/com/example/paginesgrogues/RestaurantsActivity.java
+++ app/src/main/java/com/example/paginesgrogues/RestaurantsActivity.java
@@ -2,6 +2,7 @@ package com.example.paginesgrogues;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,8 +17,18 @@ import java.io.InputStreamReader;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
+//Aqui estem declarant els tres noms de restaurants
+    TextView r1nomrestaurant,r2nomrestaurant,r3nomrestaurant;
+    //Aqui estem declarant les tres desciprcions dels restaurants
+    TextView r1descripcio,r2descripcio,r3descripcio;
+    //Aqui estem declarant les tres pagines webs dels restaurants
+    TextView r1linkpaginaweb,r2linkpaginaweb,r3linkpaginaweb;
+    //Aqui estem declarant els tres telefons dels restaurants
+    TextView r1telefon,r2telefon,r3telefon;
+    //Aqui estem declarant les tres imatges dels restaurants
+    ImageView r1imatge,r2imatge,r3imatge;
 
-    TextView t1,t2,t3,d1,d2,d3;
+
     Spinner spinnerRestaurants;
 
     @Override
@@ -26,13 +37,27 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         //Aqui farem la assignacio de valors als texteView dels noms del restaurant
-        t1 = findViewById(R.id.titol1);
-        t2 = findViewById(R.id.titol2);
-        t3 = findViewById(R.id.titol3);
+        r1nomrestaurant = findViewById(R.id.r1nomrestaurant);
+        r2nomrestaurant = findViewById(R.id.r2nomrestaurant);
+        r3nomrestaurant = findViewById(R.id.r3nomrestaurant);
         //Aqui farem la assignacio de valors als textsViews de les descripcions
-        d1 = findViewById(R.id.descripcio1);
-        d2 = findViewById(R.id.descripcio2);
-        d3 = findViewById(R.id.descripcio3);
+        r1descripcio = findViewById(R.id.r1descripcio);
+        r2descripcio = findViewById(R.id.r2descripcio);
+        r3descripcio = findViewById(R.id.r3descripcio);
+        //Aqui fare la assignacio de valors als textsViews dels links de la pagina dels restaurants
+        r1linkpaginaweb = findViewById(R.id.r1linkpaginaweb);
+        r2linkpaginaweb = findViewById(R.id.r2linkpaginaweb);
+        r3linkpaginaweb = findViewById(R.id.r3linkpaginaweb);
+        //Aqui fare la assignacio de valors als textsViews als telefons dels restaurants
+        r1telefon = findViewById(R.id.r1telefon);
+        r2telefon = findViewById(R.id.r2telefon);
+        r3telefon = findViewById(R.id.r3telefon);
+        //Aqui fare la assignacio de valors als ViewsImage per les imatges dels restaurants
+        r1imatge = findViewById(R.id.r1imatge);
+        r2imatge = findViewById(R.id.r2imatge);
+        r3imatge = findViewById(R.id.r3imatge);
+
+
 
         spinnerRestaurants = (Spinner) findViewById(R.id.spinner_tipus_restaurants);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -64,17 +89,55 @@ public class RestaurantsActivity extends AppCompatActivity {
 
             }
             exemple = new JSONArray(sb.toString());
-            String  numero = "ii";
-            String restauranttrobat;
+            String nomimatge;
             if(spinnerRestaurants.getSelectedItem().toString().equals("--Tria tipus Restaurants--")){
                 int contador = 1;
                 for(int i=0;i<exemple.length();i++){
-                    restauranttrobat = exemple.getJSONObject(i).get("tipurestaurant").toString();
+
                     if(exemple.getJSONObject(i).get("tipurestaurant").equals("MenjarRapid")){
                         switch(contador){
-                            case 1:  t1.setText((CharSequence) exemple.getJSONObject(i).get("nomRestaurant").toString());contador++;break;
-                            case 2:  t2.setText((CharSequence) exemple.getJSONObject(i).get("nomRestaurant").toString());contador++;break;
-                            case 3:  t3.setText((CharSequence) exemple.getJSONObject(i).get("nomRestaurant").toString());contador++;break;
+                            case 1:
+                                r1nomrestaurant.setText((CharSequence) exemple.getJSONObject(i).get("nomRestaurant").toString());
+                                r1descripcio.setText((CharSequence) exemple.getJSONObject(i).get("descripcio").toString());
+                                r1linkpaginaweb.setText((CharSequence) exemple.getJSONObject(i).get("linkpaginaweb").toString());
+                                r1telefon.setText((CharSequence) exemple.getJSONObject(i).get("telefon").toString());
+
+                                switch (exemple.getJSONObject(i).get("imatge").toString()){
+                                    case "./kfc":r1imatge.setImageResource(R.drawable.kfc);break;
+                                    case "./viena":r1imatge.setImageResource(R.drawable.viena);break;
+                                    case "./mcdonalds":r1imatge.setImageResource(R.drawable.mcdonalds);break;
+                                    default: break;
+                                }
+                            contador++;
+                            break;
+                            case 2:
+                                r2nomrestaurant.setText((CharSequence) exemple.getJSONObject(i).get("nomRestaurant").toString());
+                                r2descripcio.setText((CharSequence) exemple.getJSONObject(i).get("descripcio").toString());
+                                r2linkpaginaweb.setText((CharSequence) exemple.getJSONObject(i).get("linkpaginaweb").toString());
+                                r2telefon.setText((CharSequence) exemple.getJSONObject(i).get("telefon").toString());
+                                switch (exemple.getJSONObject(i).get("imatge").toString()){
+                                    case "./kfc":r2imatge.setImageResource(R.drawable.kfc);break;
+                                    case "./viena":r2imatge.setImageResource(R.drawable.viena);break;
+                                    case "./mcdonalds":r2imatge.setImageResource(R.drawable.mcdonalds);break;
+                                    default: break;
+                                }
+
+                            contador++;
+                            break;
+                            case 3:
+                                r3nomrestaurant.setText((CharSequence) exemple.getJSONObject(i).get("nomRestaurant").toString());
+                                r3descripcio.setText((CharSequence) exemple.getJSONObject(i).get("descripcio").toString());
+                                r3linkpaginaweb.setText((CharSequence) exemple.getJSONObject(i).get("linkpaginaweb").toString());
+                                r3telefon.setText((CharSequence) exemple.getJSONObject(i).get("telefon").toString());
+                                nomimatge = exemple.getJSONObject(i).get("imatge").toString();
+                                switch (nomimatge){
+                                    case "./kfc":r3imatge.setImageResource(R.drawable.kfc);break;
+                                    case "./viena":r3imatge.setImageResource(R.drawable.viena);break;
+                                    case "./mcdonalds":r3imatge.setImageResource(R.drawable.mcdonalds);break;
+                                    default: break;
+                                }
+                            contador++;
+                            break;
                             default: break;
                         }
                     }
