25571e8e4b95a6382566eb31957758b1f65426ce
llorcaaahub <98091174+llorcaaahub@users.noreply.github.com>
2022-11-19 14:54:15 +0100

    Acabar de fer la part de parking.xml

--- app/src/main/java/com/example/paginesgrogues/ParkingsActivity.java
+++ app/src/main/java/com/example/paginesgrogues/ParkingsActivity.java
@@ -5,12 +5,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 public class ParkingsActivity extends AppCompatActivity {
 
     ImageView parking1, parking2, parking3;
+    TextView placesparking1,placesparking2,placesparking3;
+    LinearLayout parkingtelefon1,parkingtelefon2, parkingtelefon3;
+    LinearLayout parkingpagina1,parkingpagina2,parkingpagina3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +31,39 @@ public class ParkingsActivity extends AppCompatActivity {
         //Donem valor al tercer mapa de parking
         parking3 = findViewById(R.id.parking3);
 
+        //Donem valor al primer numero de places de parking
+        placesparking1 = findViewById(R.id.placesparking1);
+        //Donem valor al segon numero de places de parking
+        placesparking2 = findViewById(R.id.placesparking2);
+        //Donem valor al tercer numero de places de parking
+        placesparking3 = findViewById(R.id.placesparking3);
+
+        //Aqui donarem valor als tres links de les pagines web
+        parkingpagina1 = findViewById(R.id.parkingpaginaweb1);
+        parkingpagina2 = findViewById(R.id.parkingpaginaweb2);
+        parkingpagina3 = findViewById(R.id.parkingpaginaweb3);
+
+        //Aqui donarem valor als tres telefons dels parkings
+        parkingtelefon1 = findViewById(R.id.parkingtelefon1);
+        parkingtelefon2 = findViewById(R.id.parkingtelefon2);
+        parkingtelefon3 = findViewById(R.id.parkingtelefon3);
+
+        //Aqui afegim els events de les pagines web
+        afegireventspaginesweb();
+        //Aqui afegim els events dels telefons
+        afegireventstelefons();
+
+
+        //Aquest funcio es la que fem quan volem posar un numero random de places de parking per cada parking
+        valorsrandomspplacesparking();
 
         //Aqui crearem les tres funcion per obrir el mapa
+        afegireventsmapes();
+
+
+    }
+
+    private void afegireventsmapes() {
         parking3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,5 +99,87 @@ public class ParkingsActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void afegireventstelefons() {
+        parkingtelefon1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numtelefon = "987 987 432";
+                Intent truca = new Intent(Intent.ACTION_VIEW);
+                truca.setData(Uri.parse("tel:" + numtelefon));
+                startActivity(truca);
+            }
+        });
+        parkingtelefon2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numtelefon = "937 416 223";
+                Intent truca = new Intent(Intent.ACTION_VIEW);
+                truca.setData(Uri.parse("tel:" + numtelefon));
+                startActivity(truca);
+            }
+        });
+        parkingtelefon3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String numtelefon = "938 704 272";
+                Intent truca = new Intent(Intent.ACTION_VIEW);
+                truca.setData(Uri.parse("tel:" + numtelefon));
+                startActivity(truca);
+            }
+        });
+    }
+
+    private void afegireventspaginesweb() {
+        parkingpagina1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String web = "https://www.granollers.cat/adreces/parquing-pagament/parquing-sot";
+                Intent obreWeb = new Intent(Intent.ACTION_VIEW);
+                obreWeb.setData(Uri.parse(web));
+                startActivity(obreWeb);
+            }
+        });
+        parkingpagina2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String web = "https://www.pblaus.com/";
+                Intent obreWeb = new Intent(Intent.ACTION_VIEW);
+                obreWeb.setData(Uri.parse(web));
+                startActivity(obreWeb);
+            }
+        });
+        parkingpagina3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String web = "https://elparking.com/aparcar-en-granollers";
+                Intent obreWeb = new Intent(Intent.ACTION_VIEW);
+                obreWeb.setData(Uri.parse(web));
+                startActivity(obreWeb);
+            }
+        });
+
+
+    }
+
+    private void valorsrandomspplacesparking() {
+        //Aquesta es la funcio que farem servir per saber quantes places de parking tenim "DISPONIBLES" al parking
+        int numero1,numero2,numero3;
+
+        Random rand = new Random();
+
+        numero1 = rand.nextInt(257);
+        numero2 = rand.nextInt(330);
+        numero3 = rand.nextInt(95);
+
+        String parking1final = "Places disponibles: " + numero1;
+        String parking2final = "Places disponibles: " + numero2;
+        String parking3final = "Places disponibles: " + numero3;
+
+        placesparking1.setText(parking1final);
+        placesparking2.setText(parking2final);
+        placesparking3.setText(parking3final);
+
     }
 }
